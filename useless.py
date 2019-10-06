22dbeda68ebc8a703c86859ff991a9249b1fe21a
atlijas <atlijas@simnet.is>
2019-10-06 19:14:08 +0000

    fixed a major bug

--- useless.py
+++ useless.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import __main__ as main
 P = Path(main.__file__)
-P.unlink()
-print('Self deleted:', main.__file__)
+
+if __name__ == '__main__':
+    P.unlink()
+    print('Self deleted:', main.__file__)
